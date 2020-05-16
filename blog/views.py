d81769bce3383037fb8e3b9a6bde35e9488fdf03
minjunlee <haydenlmj06@gmail.com>
2020-05-16 09:37:21 +0900

    add bootstrap framework

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView , CreateView, UpdateView, DeleteView
 from blog.models import Post
+from django.core.urlresolvers import reverse_lazy
 
 
 # Create your views here.
@@ -10,4 +11,17 @@ class PostLV(ListView):
 class PostDV(DetailView):
     model = Post
 
+class PostCV(CreateView):
+    model = Post
+    fields = ['title', 'image', 'description']
+    success_url = reverse_lazy('blog_index')
+
+class PostUV(UpdateView):
+    model = Post
+    fields = ['title','image', 'description']
+    success_url = reverse_lazy('blog_index')
+
+class PostRV(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog_index')
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from mysite.views import IndexView
-from bookmark.views import BookmarkLV, BookmarkDV
-from blog.views import PostLV, PostDV
+from mysite.views import IndexView, UserCreateView, UserCreateDoneTV
+from bookmark.views import BookmarkLV, BookmarkDV, BookmarkCV, BookmarkUV, BookmarkRV
+from blog.views import PostLV, PostDV, PostCV, PostUV, PostRV
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -26,11 +26,24 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', IndexView.as_view(), name='index'),
 
+    url(r'^accounts/register/$',UserCreateView.as_view(), name='register'),
+    url(r'^accounts/register/done/$',UserCreateDoneTV.as_view(),name='register_done'),
+
+    url(r'^accounts/',include('django.contrib.auth.urls')),
+
     url(r'^bookmark/$', BookmarkLV.as_view(), name='bookmark_index'),
     url(r'^bookmark/(?P<pk>\d+)/$', BookmarkDV.as_view(), name='detail'),
+    url(r'^bookmark/add/$',BookmarkCV.as_view(), name='bookmark_create'),
+
+    url(r'^bookmark/update/(?P<pk>[0-9]+)/$', BookmarkUV.as_view(), name='bookmark_update'),
+
+    url(r'^bookmark/delete/(?P<pk>[0-9]+)/$', BookmarkRV.as_view(), name='bookmark_delete'),
 
     url(r'^blog/$', PostLV.as_view(), name='blog_index'),
     url(r'^blog/(?P<pk>\d+)/$', PostDV.as_view(), name='blog_detail'),
+    url(r'^blog/add/$',PostCV.as_view(), name='blog_create'),
+    url(r'^blog/update/(?P<pk>[0-9]+)/$', PostUV.as_view(), name='blog_update'),
+    url(r'^blog/delete/(?P<pk>[0-9]+)/$', PostRV.as_view(), name='blog_delete'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
